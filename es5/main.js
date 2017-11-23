e24e44e25327954ebf9262cb51797e3b187bcbf0
syedtajudheen <syedtajudheen@live.com>
2017-11-23 18:49:11 +0530

    refactoring

--- es5/main.js
+++ es5/main.js
@@ -1,9 +1,12 @@
-'use strict';
+"use strict";
+
+/**********************************************************TO-DO LIST********************************************************************/
+var FETCH_URL = "https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc";
+var POST_URL = "https://jsonprovider.herokuapp.com/todos/";
 
 $(function () {
-
-    //using promise fetch(url)
-    var promise = fetch('https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc');
+    //USING PROMISE FETCH(URL)
+    var promise = fetch(FETCH_URL);
     promise.then(function (response) {
         return response.json();
     }).then(function (data) {
@@ -14,14 +17,14 @@ $(function () {
     }).catch(function (error) {
         return console.log('BAD', error);
     });
-
+    //CLICK_FUNCTION
     $("#click").click(function () {
         var todo = document.getElementById("gettodo").value;
         console.log(todo);
         //to post data using ajax
         $.ajax({
             type: 'POST',
-            url: 'https://jsonprovider.herokuapp.com/todos/',
+            url: POST_URL,
             data: {
                 userID: 1,
                 title: todo,
--- es6/main.js
+++ es6/main.js
@@ -1,33 +1,32 @@
+/**********************************************************TO-DO LIST********************************************************************/
+const FETCH_URL= "https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc";
+const POST_URL="https://jsonprovider.herokuapp.com/todos/";
 
 $(() => {
-    
-            //using promise fetch(url)
-            let promise =  fetch('https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc');
-            promise.then(response => response.json())
-            .then(data => {
-                $.each(data,(key,value) => {
-                    $("#checkbox").prepend('<li><input type="checkbox" id="checkit" class="done"/>&nbsp' + value.title );
-                    $("#checkit").prop('checked', value.completed);
+                //USING PROMISE FETCH(URL)
+                let promise =  fetch(FETCH_URL);
+                promise.then(response => response.json())
+                .then(data => {
+                    $.each(data,(key,value) => {
+                        $("#checkbox").prepend('<li><input type="checkbox" id="checkit" class="done"/>&nbsp' + value.title );
+                        $("#checkit").prop('checked', value.completed);
+                    })
                 })
-            })
-            .catch(error => console.log('BAD' , error))
-        
-    $("#click").click(() => {
-                    let todo = document.getElementById("gettodo").value;
-                    console.log(todo);
-                    //to post data using ajax
-                   $.ajax({
-                        type: 'POST',
-                        url: 'https://jsonprovider.herokuapp.com/todos/',
-                        data: {
-                            userID: 1,
-                            title: todo,
-                            completed: false,
-                        },
-                        success: ((data) =>  console.log("posted successfully", data) )       
-                    });
-                    
-                    
-    });
-    
-})
+                .catch(error => console.log('BAD' , error))
+                //CLICK_FUNCTION
+                $("#click").click(() => {
+                                let todo = document.getElementById("gettodo").value;
+                                console.log(todo);
+                                //to post data using ajax
+                                $.ajax({
+                                    type: 'POST',
+                                    url: POST_URL,
+                                    data: {
+                                        userID: 1,
+                                        title: todo,
+                                        completed: false,
+                                    },
+                                    success: ((data) =>  console.log("posted successfully", data) )       
+                                });                     
+                });        
+});
