d39dc784ba228730756c5a7c6c36f5c0e44296b6
syedtajudheen <syedtajudheen@live.com>
2017-11-24 19:00:28 +0530

    new files

--- es5/main.js
+++ es5/main.js
@@ -1,37 +1,55 @@
 "use strict";
 
-/**********************************************************TO-DO LIST********************************************************************/
 var FETCH_URL = "https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc";
 var POST_URL = "https://jsonprovider.herokuapp.com/todos/";
 
-$(function () {
+$('#gettodo').keypress(function (event) {
+    event.stopPropagation();
+    var keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode == '13') {
+        if ($(this).val() !== '') {
+            var todo = $(this).val();
+            createTodo(todo);
+        } else {
+            alert("alert");
+        }
+    }
+});
+
+(function ($) {
     //USING PROMISE FETCH(URL)
     fetch(FETCH_URL).then(function (response) {
         return response.json();
     }).then(function (data) {
         $.each(data, function (key, value) {
-            $("#checkbox").prepend('<li><input type="checkbox" id="checkit" class="done"/>&nbsp' + value.title);
-            $("#checkit").prop('checked', value.completed);
+            $("#checkbox").prepend('<li><input type="checkbox" id="mycheckbox" class="done"/>&nbsp' + value.title);
+            $("#mycheckbox").prop('checked', value.completed);
         });
     }).catch(function (error) {
         return console.log('BAD', error);
     });
-    //CLICK_FUNCTION
-    $("#click").click(function () {
-        var todo = document.getElementById("gettodo").value;
-        console.log(todo);
-        //to post data using ajax
-        $.ajax({
-            type: 'POST',
-            url: POST_URL,
-            data: {
-                userID: 1,
-                title: todo,
-                completed: false
-            },
-            success: function success(data) {
-                return console.log("posted successfully", data);
-            }
-        });
+})(jQuery);
+
+var createTodo = function createTodo(text) {
+    var todo = $('#gettodo').val();
+    //console.log(todo);
+
+    $.ajax({
+        type: 'POST',
+        url: POST_URL,
+        data: {
+            userID: 1,
+            title: todo,
+            completed: true
+        },
+        success: function success(data) {
+            console.log("posted successfully", data);
+        }
     });
-});
+
+    {
+        var markup = '<li> <input type="checkbox" id="mycheckbox" class="done"/>' + text;
+        $('#checkbox').prepend(markup);
+        $('#gettodo').val('');
+    }
+};jQuery;
--- es6/main.js
+++ es6/main.js
@@ -1,31 +1,65 @@
-/**********************************************************TO-DO LIST********************************************************************/
-const FETCH_URL= "https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc";
-const POST_URL="https://jsonprovider.herokuapp.com/todos/";
 
-$(() => {
-                //USING PROMISE FETCH(URL)
-                 fetch(FETCH_URL).then(response => response.json())
-                .then(data => {
-                    $.each(data,(key,value) => {
-                        $("#checkbox").prepend('<li><input type="checkbox" id="checkit" class="done"/>&nbsp' + value.title );
-                        $("#checkit").prop('checked', value.completed);
-                    })
-                })
-                .catch(error => console.log('BAD' , error))
-                //CLICK_FUNCTION
-                $("#click").click(() => {
-                                let todo = document.getElementById("gettodo").value;
-                                console.log(todo);
-                                //to post data using ajax
-                                $.ajax({
-                                    type: 'POST',
-                                    url: POST_URL,
-                                    data: {
-                                        userID: 1,
-                                        title: todo,
-                                        completed: false,
-                                    },
-                                    success: ((data) =>  console.log("posted successfully", data) )       
-                                });                     
-                });        
+const FETCH_URL ="https://jsonprovider.herokuapp.com/todos/?limit=50&sort=id+desc";
+const POST_URL ="https://jsonprovider.herokuapp.com/todos/"
+
+
+$('#gettodo').keypress(function (event) {
+    event.stopPropagation();
+ let keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {    
+        if ($(this).val() !== '') {
+           let todo = $(this).val();
+            createTodo(todo);
+        } 
+        else{
+            alert("alert");
+        }
+        
+       
+    }
 });
+
+
+
+
+(($) => {
+    //USING PROMISE FETCH(URL)
+    fetch(FETCH_URL).then(response => response.json())
+    .then(data => {
+        $.each(data,(key,value) => {
+            $("#checkbox").prepend('<li><input type="checkbox" id="mycheckbox" class="done"/>&nbsp' + value.title );
+            $("#mycheckbox").prop('checked', value.completed);
+        })
+    })
+    .catch(error => console.log('BAD' , error))
+})(jQuery);
+
+
+let createTodo=(text) => {
+    let todo = $('#gettodo').val();
+    //console.log(todo);
+  
+            $.ajax ({
+                type: 'POST',
+                url: POST_URL,
+                data:{
+                    userID: 1,
+                    title: todo,
+                    completed: true,
+                },
+                success: (data) => {
+                console.log("posted successfully", data); 
+                }
+            });
+    
+      {
+        var markup = '<li> <input type="checkbox" id="mycheckbox" class="done"/>' + text ;
+        $('#checkbox').prepend(markup);
+        $('#gettodo').val('');
+    }};(jQuery)
+    
+
+
+
+
+  
